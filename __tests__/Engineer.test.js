535b0d5a2397007bfef9b627649e629dab0260dc
zehra <zehradastan@hotmail.com>
2023-02-27 14:27:20 +0000

    directory structure added

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,49 +8,49 @@ test('can create new Engineer object', () => {
 })
 
 test('can set id artibute on the engineer object', () => {
-    const id = 1
+    const id = 2
     const engineer = new Engineer(id)
     expect(engineer.id).toBe(id)
 })
 
 test('can set name artibute on the engineer object', () => {
-    const name = "zehra"
-    const engineer = new Engineer(1, name)
+    const name = "javad"
+    const engineer = new Engineer(2, name)
     expect(engineer.name).toBe(name)
 })
 
 test('can set email artibute on the engineer object', () => {
-    const email = "zehra@test"
-    const engineer = new Engineer(1, "zehra", email)
+    const email = "javad@test"
+    const engineer = new Engineer(2, "javad", email)
     expect(engineer.email).toBe(email)
 })
 
 test('can set gitHub artibute on the engineer object', () => {
-    const gitHub = "ZDastan"
-    const engineer = new Engineer(1, "zehra", gitHub)
+    const gitHub = "JDastan"
+    const engineer = new Engineer(2, "javad", gitHub)
     expect(engineer.gitHub).toBe(gitHub)
 })
 
 test('can get id by calling getId()', () => {
-    const id = 1
+    const id = 2
     const engineer = new Engineer(id)
     expect(engineer.getId()).toBe(id)
 })
 
 test('can get name by calling getName()', () => {
-    const name = "zehra"
-    const engineer = new Engineer(1, name)
+    const name = "javad"
+    const engineer = new Engineer(2, name)
     expect(engineer.getName()).toBe(name)
 })
 
 test('can get email by calling getEmail()', () => {
-    const email = "zehra@test"
-    const engineer = new Engineer(1, "zehra", email)
+    const email = "javad@test"
+    const engineer = new Engineer(2, "javad", email)
     expect(engineer.getEmail()).toBe(email)
 })
 
 test('can get gitHub by calling getGitHub()', () => {
-    const gitHub = "ZDastan"
-    const engineer = new Engineer(1, "zehra", gitHub)
+    const gitHub = "JDastan"
+    const engineer = new Engineer(2, "javad", gitHub)
     expect(engineer.getGitHub()).toBe(gitHub)
 })
